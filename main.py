beb766babc57af158b8c270aa7e4192f2e115083
heregeN0C <alysoncara5@gmail.com>
2022-09-19 16:53:26 -0300

    initial

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-
-import matplotlib.pyplot as plt
+from bokeh.plotting import figure, show
 import requests
+from datetime import datetime
 
-url = 'https://lunio.cloudprovtel.com/api_jsonrpc.php'
-token = 'eff5bc90efacc75bd31505b5a724b0bb'
+
+url = 'http://mob.cloudprovtel.com/api_jsonrpc.php'
+token = '55a634b9b4ad9256afe7a8081a4a26283418acb3a9301b18699beeb9df52efb6'
 
 def filtroReport():
 
@@ -12,8 +13,8 @@ def filtroReport():
         "method": "history.get",
         "params": {
             "output": "extend",
-            "hostids": "10295",
-            "itemids": "49470",
+            "hostids": "10667",
+            "itemids": "142368",
             "sortfield": "clock",
             "limit": 10,
         },
@@ -26,11 +27,17 @@ def filtroReport():
     return r.json()
 
 res = filtroReport()
+#print(res)
+
 valores = []
+tempo = []
 for y in range(len(res['result'])):
     valores.append(res['result'][y]['value'])
-#print(valores)
+    tempo.append(res['result'][y]['clock'])
 
-for i in range(len(valores)):
-    plt.plot(valores)
-plt.show()
+
+p = figure(title="teste", x_axis_label='data/hora', y_axis_label='valor')
+
+p.line(tempo, valores, line_width=1)
+show(p)
+#print(valores)
