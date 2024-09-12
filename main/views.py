c20f78153e1f53618eec6ce8b73c5c9a92dd0b66
heregeN0C <alysoncara5@gmail.com>
2024-09-12 15:06:50 -0300

    adicionada query de hostgroup

--- main/views.py
+++ main/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pyzabbix import ZabbixAPI
+
 
 # Create your views here.
 
+token = "888f5efeca7d1cda0ecc2b468f284ea41e8cc28889730abd5a2f3b41fca2a586"
+server = "https://lunio.cloudprovtel.com/"
+
+zapi = ZabbixAPI(server)
+zapi.login(api_token=token)
+
 def mainPage(request):
-    return render(request, 'main.html')
+    if not zapi.is_authenticated:
+        zapi.login(api_token=token)
+        version = zapi.api_version()
+        #coletar grupos de hosts
+        try:
+            payload = {
+                "output": ["groupid", "name"]
+            }
+            r = zapi.do_request("hostgroup.get", payload)
+            group_list = r["result"]
+            #print(group_list)
+            return render(request, 'main.html', context={"version": version, "group_list": group_list})
+
+        except Exception as e:
+            print("Erro ao executar o metodo: ", e)
+
+
+    else:
+        version = zapi.api_version()
+        # coletar grupos de hosts
+        try:
+            payload = {
+                "output": ["groupid", "name"]
+            }
+            r = zapi.do_request("hostgroup.get", payload)
+            group_list = r["result"]
+            #print(group_list)
+            return render(request, 'main.html', context={"version": version, "group_list": group_list})
+
+        except Exception as e:
+            print("Erro ao executar o metodo: ", e)
+
